03dc6c903ccd5aa66d6e494770302638ba94f754
Administrator <admin@bfastreader.local>
2026-06-24 22:37:20 +0800

    chore: bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bquantum.bfastreader"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.2.2"
+        versionCode = 4
+        versionName = "0.3.0"
     }
 
     buildTypes {
